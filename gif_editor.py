e8140d0525c9baf5a22183b3f375dec10ad7c381
Jin Heng Yap <jinhengyap@gmail.com>
2020-12-30 18:41:30 +0800

    Implement embedding and extracting data in GIFs

--- gif_editor.py
+++ gif_editor.py
@@ -87,7 +87,7 @@ def preprocess(fp: str, out: str) -> None:
     queue = deque()
     gifsicle_commands = ["gifsicle"]
     magick_command = ["./magick", "identify", "-verbose", None]
-    pattern = re.compile(r"(?<=Transparent color: )(\w)+(\(.+\))?")
+    pattern = re.compile(r"(?<=Transparent color: )\w+(\(.+\))?")
     with GIFImage(filename=fp) as wand_im:
         with PIL.Image.open(fp) as pil_im:
             pil_iter = PIL.ImageSequence.Iterator(pil_im)
@@ -96,7 +96,7 @@ def preprocess(fp: str, out: str) -> None:
                 wand_frame, wand_clone = wand_im.clone_frame(frame_no)
                 cur_frame_no = frame_no * 2
                 clone_frame_no = cur_frame_no + 1
-                # print(frame_no)
+                print(frame_no)
                 trans_idx = pil_frame.info.get("transparency")
                 if trans_idx is not None:
                     magick_command[-1] = f"{fp}[{frame_no}]"
@@ -142,23 +142,118 @@ def preprocess(fp: str, out: str) -> None:
     subprocess.run(gifsicle_commands)
 
 
-def random_bitstream(rng: np.random.Generator = None) -> Iterator[int]:
+def embed_data(fp: str, out: str, data: Iterable[str], k: int = None) -> str:
+    data = iter(data)
+    cur_data = deque()
+    gifsicle_commands = ["gifsicle"]
+    magick_command = ["./magick", "identify", "-verbose", None]
+    pattern = re.compile(r"(?<=Transparent color: )\w+(\(.+\))?")
+    msg = []
+    with Image(filename=fp) as wand_im:
+        wand_seq = wand_im.sequence
+        with PIL.Image.open(fp) as pil_im:
+            for frame_no in range(1, pil_im.n_frames, 2):
+                magick_command[-1] = f"{fp}[{frame_no}]"
+                search_val = subprocess.run(magick_command, capture_output=True, encoding="utf-8").stdout
+                trans_color_name = pattern.search(search_val)[0]
+                trans_arr = re.search(f"(?<=: ).+(?={re.escape(trans_color_name)})", search_val)[0].split()[0][1:-1].split(",")
+                try:
+                    trans_arr = np.array([int(i) for i in trans_arr])
+                except TypeError:
+                    trans_arr = np.zeros(4, int)
+                prev_frame_no = frame_no - 1
+                pil_im.seek(prev_frame_no)
+                prev_trans_idx = pil_im.info.get("transparency")
+                pil_im.seek(frame_no)
+                cur_trans_idx = pil_im.info["transparency"]
+                with wand_seq[prev_frame_no].clone() as prev_frame:
+                    prev_arr = np.array(prev_frame)
+                print(frame_no, trans_arr)
+                modified = False
+                with wand_seq[frame_no].clone() as cur_frame:
+                    cur_arr = np.array(cur_frame)
+                    for i, row in enumerate(cur_arr):
+                        for j, _ in enumerate(row):
+                            if prev_trans_idx is None or not np.array_equal(prev_arr[i, j], trans_arr):
+                                if k is not None:
+                                    if not cur_data:
+                                        newdata = [next(data) for _ in range(k)]
+                                        cur_data.extend(rzl(newdata, k))
+                                        msg.extend(newdata)
+                                    next_bit = cur_data.popleft()
+                                else:
+                                    next_bit = next(data)
+                                    msg.append(next_bit)
+                                if next_bit == "0":
+                                    cur_arr[i, j] = trans_arr
+                                    modified = True
+                    if modified:
+                        with Image.from_array(cur_arr) as new_frame:
+                            cur_frame.import_pixels(channel_map="RGBA", data=new_frame.export_pixels())
+                        wand_seq[frame_no] = cur_frame
+                prev_trans_command = f"-t{prev_trans_idx}" if prev_trans_idx is not None else "--no-transparent"
+                gifsicle_commands.extend((prev_trans_command, f"{out}", f"#{prev_frame_no}", f"-t{cur_trans_idx}", f"{out}", f"#{frame_no}"))
+            pil_im.seek(pil_im.n_frames - 1)
+            trans_idx = pil_im.info.get("transparency")
+            last_command = f"-t{trans_idx}" if trans_idx is not None else "--no-transparent"
+            gifsicle_commands.extend((last_command, f"{out}", "#-1", "-o", f"{out}"))
+        wand_im.save(filename=out)
+    subprocess.run(gifsicle_commands)
+    msg = "".join(msg)
+    return msg[:-k] if cur_data else msg
+
+
+def extract_payload(fp: str, k: int = None) -> str:
+    magick_command = ["./magick", "identify", "-verbose", None]
+    pattern = re.compile(r"(?<=Transparent color: )\w+(\(.+\))?")
+    payload = []
+    with Image(filename=fp) as wand_im:
+        wand_seq = wand_im.sequence
+        with PIL.Image.open(fp) as pil_im:
+            for frame_no in range(1, pil_im.n_frames, 2):
+                magick_command[-1] = f"{fp}[{frame_no}]"
+                search_val = subprocess.run(magick_command, capture_output=True, encoding="utf-8").stdout
+                trans_color_name = pattern.search(search_val)[0]
+                trans_arr = re.search(f"(?<=: ).+(?={re.escape(trans_color_name)})", search_val)[0].split()[0][1:-1].split(",")
+                try:
+                    trans_arr = np.array([int(i) for i in trans_arr])
+                except TypeError:
+                    trans_arr = np.zeros(4, int)
+                prev_frame_no = frame_no - 1
+                pil_im.seek(prev_frame_no)
+                prev_no_trans = "transparency" not in pil_im.info
+                with wand_seq[prev_frame_no].clone() as prev_frame:
+                    prev_arr = np.array(prev_frame)
+                print(frame_no, trans_arr)
+                with wand_seq[frame_no].clone() as cur_frame:
+                    cur_arr = np.array(cur_frame)
+                for i, row in enumerate(cur_arr):
+                    for j, col in enumerate(row):
+                        if prev_no_trans or not np.array_equal(prev_arr[i, j], trans_arr):
+                            payload.append("0" if np.array_equal(col, trans_arr) else "1")
+    if k is not None:
+        while payload and payload[-1] == "0":
+            payload.pop()
+        payload = extract_rzl(payload, k)
+    return "".join(payload)
+
+
+def random_bitstream(rng: np.random.Generator = None) -> Iterator[str]:
     if rng is None:
         rng = np.random.default_rng()
     while True:
-        yield rng.integers(2)
+        yield str(rng.integers(2))
 
 
 def rzl(data: Iterable[Union[int, str]], k: int) -> Iterator[str]:
     res = iter([])
-    data = list(data)
+    data = "".join(str(char) for char in data)
     for i in range(0, len(data), k):
-        d = "".join(str(chr) for chr in data[i:i+k])
-        res = itertools.chain(res, ("0" for _ in range(int(d, base=2))), "1")
+        res = itertools.chain(res, ("0" for _ in range(int(data[i:i+k], base=2))), "1")
     return res
 
 
-def extract_rzl(data: Iterable[str], k: int) -> Iterable[str]:
+def extract_rzl(data: Iterable[str], k: int) -> Iterator[str]:
     data = "".join(data).split("1")
     data.pop()
     return itertools.chain(*[iter(format(len(i), f"0{k}b")) for i in data])
