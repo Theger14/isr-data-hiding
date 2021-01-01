89984ce80e444d033f6302cbc2c79ff3fb52166c
Jin Heng Yap <jinhengyap@gmail.com>
2021-01-02 05:05:08 +0800

    Convert program into command line app

--- main.py
+++ main.py
@@ -1,15 +1,31 @@
+import argparse
+
 from gif_hider import GIFHider, random_bitstream
 from numpy.random import default_rng
 
 
+def parse_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(
+        description="Perform data hiding in a GIF file"
+    )
+    parser.add_argument("file", help="input GIF file")
+    parser.add_argument("out", help="output GIF file")
+    parser.add_argument(
+        "-v",
+        "--validate",
+        help="validate data extracted is correct",
+        action="store_true"
+    )
+    return parser.parse_args()
+
+
 def main() -> None:
-    file = "Original/Draw1.gif"
-    out = "out.gif"
+    args = parse_args()
     stream = random_bitstream(default_rng(0))
-    with GIFHider(file, out) as hider:
+    with GIFHider(args.file, args.out) as hider:
         hider.preprocess()
         hider.embed_data(stream, 3)
-        hider.extract_payload(True)
+        hider.extract_payload(args.validate)
 
 
 if __name__ == "__main__":
